4cd7e2405fa15c8e3552c4d5b61d85f017238a47
miguelapv48 <miguelapv48@educastur.es>
2023-04-20 17:56:35 +0200

    20 de Abril

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
      *
      * @var string<int, string>
      */
+    public $timestamps = false;
     protected $fillable = [
         'name', 'email', 'password',
     ];
