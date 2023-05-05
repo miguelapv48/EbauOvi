bf5c83eb5b90de0223c858c30ef8d6691ab13dee
miguelapv48 <miguelapv48@educastur.es>
2023-05-05 16:40:17 +0200

    5 Mayo

--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -4,9 +4,8 @@
             {{ __('Bienvenido a las noticias de EbauOvi') }}
         </h2>
     </x-slot>
-    
-    <div class="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
-        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">       
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> 
             @foreach ($noticias as $noticia)
                 <article class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2  @endif">
                     <div class="w-full h-full px-8 flex flex-col justify-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
     ],
 
     theme: {
+        screens: {
+            sm:'640px',
+            md:'768px',
+            lg:'1024px',
+            xl:'1280px',
+        },
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
