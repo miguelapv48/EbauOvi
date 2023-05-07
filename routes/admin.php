71f1867d632862be13813cc8fbcccacd3062237e
miguelapv48 <miguelapv48@educastur.es>
2023-05-07 12:25:19 +0200

    Proteccion de rutas para el administrador

--- routes/admin.php
+++ routes/admin.php
@@ -7,10 +7,13 @@ use App\Http\Controllers\Admin\PreguntasController;
 use App\Http\Controllers\Admin\TesstController;
 use App\Http\Controllers\Admin\AsignaturasController;
 
-Route::get('',[HomeController::class,'index'])->middleware('can:admin.home')->name('admin.home');
 
-Route::resource('users',UsuariosController::class)->names('admin.users');
-Route::resource('noticias',NoticiasController::class)->names('admin.noticias');
-Route::resource('preguntas',PreguntasController::class)->names('admin.preguntas');
-Route::resource('tesst',TesstController::class)->names('admin.tesst');
-Route::resource('asignaturas',AsignaturasController::class)->names('admin.asignaturas');
+Route::middleware('can:admin')->group(function () {
+    Route::get('', [HomeController::class, 'index'])->name('admin.home');
+
+    Route::resource('users', UsuariosController::class)->names('admin.users');
+    Route::resource('noticias', NoticiasController::class)->names('admin.noticias');
+    Route::resource('preguntas', PreguntasController::class)->names('admin.preguntas');
+    Route::resource('tesst', TesstController::class)->names('admin.tesst');
+    Route::resource('asignaturas', AsignaturasController::class)->names('admin.asignaturas');
+});
