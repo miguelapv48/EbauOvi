78fadad4d41e76b52e9144c7fb286f3ae1fa06d9
miguelapv48 <miguelapv48@educastur.es>
2023-05-08 17:51:12 +0200

    Estilar pagina de asignaturas

--- resources/views/asignaturas.blade.php
+++ resources/views/asignaturas.blade.php
@@ -5,13 +5,12 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @foreach($asignaturas as $asignatura)
-                    <a href="{{ route('examenes',['id' =>$asignatura->id]) }}">{{ $asignatura->nombre }}</a>
-                @endforeach
-            </div>
+    <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8 flex gap-4">
+        @foreach($asignaturas as $asignatura)
+        <div class="bg-white shadow-xl sm:rounded-lg p-4">
+            <h3 class="text-lg">{{ $asignatura->nombre }}</h3>
+            <a class="block bg-indigo-500 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-4 rounded mt-4" href="{{ route('examenes',['id' =>$asignatura->id]) }}">Ver examenes</a>
         </div>
+        @endforeach
     </div>
 </x-app-layout>
